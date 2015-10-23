2c820a4af7e2f59cb47b8148862ed18f50d0c062
LuboYankov <ahhak123@gmail.com>
2015-10-23 17:04:16 +0300

    Adding comments

--- 01_SamleJava/src/org/elsysbg/ip/java/TextInput.java
+++ 01_SamleJava/src/org/elsysbg/ip/java/TextInput.java
@@ -6,9 +6,13 @@ public class TextInput {
 
 	public static void main(String[] args) {
 		System.out.print("Enter your name: ");
+		// standard input is held by System.in
 		final Scanner in = new Scanner(System.in);
+		// get text from standard input
+		// scanner is used
 		final String name = in.next();
 		System.out.println("Hello, " + name);
+		// closing the scanner
 		in.close();
 	}
 
