755c1848ae80bb1c48169da0d48b58d3cc25da86
LuboYankov <ahhak123@gmail.com>
2015-11-06 18:19:41 +0200

    Added multithreading

--- 02_SampleSockets/src/org/elsysbg/ip/sockets/ClientHandler.java
+++ 02_SampleSockets/src/org/elsysbg/ip/sockets/ClientHandler.java
@@ -5,15 +5,18 @@ import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ClientHandler {
+public class ClientHandler implements Runnable {
 
 	private final Socket socket;
 	private static final String COMMAND_STOP_SERVER = "stopServer";
+	private final EchoServer echoServer;
 	
-	public ClientHandler(Socket socket) {
+	public ClientHandler(Socket socket, EchoServer echoServer) {
 		this.socket = socket;
+		this.echoServer = echoServer;
 	}
 	
+	@Override
 	public void run() {
 		try {
 			final PrintStream out = new PrintStream(socket.getOutputStream());
@@ -21,7 +24,7 @@ public class ClientHandler {
 			while(scanner.hasNextLine()) {
 				final String line = scanner.nextLine();
 				if(COMMAND_STOP_SERVER.equals(line)) {
-					break;
+					echoServer.stopServer();
 				}
 				out.println(line);
 			}
--- 02_SampleSockets/src/org/elsysbg/ip/sockets/EchoServer.java
+++ 02_SampleSockets/src/org/elsysbg/ip/sockets/EchoServer.java
@@ -5,22 +5,27 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	
+	private boolean running;
 	private final int port;
 	
 	public EchoServer(int port) {
 		this.port = port;
+		this.running = false;
 	}
 	
 	public void startServer() throws IOException {
-		boolean running = true;
+		running = true;
 		final ServerSocket serverSocket = new ServerSocket(port);
 		while(running) {
 			final Socket socket = serverSocket.accept();
-			final ClientHandler client = new ClientHandler(socket);
-			client.run(); 
+			final ClientHandler client = new ClientHandler(socket, this);
+			new Thread(client).start();
 		}
 		serverSocket.close();
 	}
 
+	public void stopServer() {
+		running = false;
+	}
+	
 }
