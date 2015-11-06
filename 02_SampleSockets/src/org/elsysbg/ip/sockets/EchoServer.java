6bee1f9dbb9fe9a642c8aca8de00d4c12b37ef83
LuboYankov <ahhak123@gmail.com>
2015-11-06 18:44:42 +0200

    Synchronized threads

--- 02_SampleSockets/src/org/elsysbg/ip/sockets/EchoServer.java
+++ 02_SampleSockets/src/org/elsysbg/ip/sockets/EchoServer.java
@@ -3,21 +3,27 @@ package org.elsysbg.ip.sockets;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class EchoServer {
 	private boolean running;
 	private final int port;
+	private final List<ClientHandler> clients = Collections.synchronizedList(new LinkedList<ClientHandler>());
 	
 	public EchoServer(int port) {
 		this.port = port;
-		this.running = false;
 	}
 	
 	public void startServer() throws IOException {
 		setRunning();
 		final ServerSocket serverSocket = new ServerSocket(port);
 		while(isRunning()) {
-			final Socket socket = serverSocket.accept();
+			final Socket socket;
+			synchronized (this) {
+				socket = serverSocket.accept();
+			}
 			final ClientHandler client = new ClientHandler(socket, this);
 			new Thread(client).start();
 		}
@@ -35,8 +41,15 @@ public class EchoServer {
 		return running;
 	}
 	
-	public synchronized void stopServer() {
+	public synchronized void stopServer() throws IOException {
 		running = false;
+		for(ClientHandler next : clients) {
+			next.stopClient();
+		}
+	}
+	
+	public synchronized void onClientsStopped(ClientHandler clientHandler) {
+		
 	}
 	
 }
