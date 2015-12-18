c09bb52105d31cda26fcf5d9eddf450bdca273ea
LuboYankov <ahhak123@gmail.com>
2015-12-18 18:13:24 +0200

    showPanel added

--- 04_SampleAjax/WebContent/scripts/todoList.js
+++ 04_SampleAjax/WebContent/scripts/todoList.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 	"use strict"
 	
+	function showPanel(panelName) {
+		var ALL_PANELS = ["emptyPanel", "readPanel", "updatePanel", "createPanel"];
+		_.forEach(ALL_PANELS, function(nextValue) {
+			$("#"+nextValue).hide();
+		});
+		$("#"+panelName).show();
+	}
+	
 	var ENDPOINT = "http://localhost:3000/tasks";
 	
 	$.ajax(ENDPOINT, {
@@ -12,10 +20,14 @@ $(document).ready(function() {
 			newItem.text(task.title);
 			newItem.addClass("list-group-item");
 			newItem.attr("data-task-id", task.id);
-			
 			$("#tasksList").append(newItem);
 		}
 		$("#taskList").html("");
 		_.forEach(response, addTaskToList);
+	});
+	
+	$(document).on("click", "#tasksList [data-task-id]", function() {
+		// var taskId = $(this).attr("data-task-id");
+		showPanel("readPanel");
 	});
 });
