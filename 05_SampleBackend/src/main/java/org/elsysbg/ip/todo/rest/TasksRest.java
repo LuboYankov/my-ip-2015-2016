aea7256a81dbb707323c595a2bbe841021f964b0
LuboYankov <ahhak123@gmail.com>
2016-02-16 10:07:30 +0200

    Sample REST endpoint - updating task on PUT

--- 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/rest/TasksRest.java
+++ 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/rest/TasksRest.java
@@ -1,12 +1,13 @@
 package org.elsysbg.ip.todo.rest;
 
-import java.util.Collections;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -43,5 +44,22 @@ public class TasksRest {
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	public Task createTask(Task task) {
 		return tasksService.createTask(task);
+	}
+	
+	@DELETE
+	@Path("/{taskId}")
+	public void deleteTask(@PathParam("taskId")long taskId) {
+		tasksService.deleteTask(taskId);
+	}
+	
+	@PUT
+	@Path("/{taskId}")
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Task updateTask(@PathParam("taskId") long taskId, Task task) {
+		final Task fromDb = tasksService.getTask(taskId);
+		fromDb.setTitle(task.getTitle());
+		fromDb.setDescription(task.getDescription());
+		return tasksService.updateTask(fromDb);
 	}
 }
