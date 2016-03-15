d7c14dd0e94a520955c7d21513be74e3c7a6d464
LuboYankov <ahhak123@gmail.com>
2016-03-15 08:29:22 +0200

    Class Member can be persisted

--- 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/entities/Member.java
+++ 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/entities/Member.java
@@ -1,9 +1,29 @@
 package org.elsysbg.ip.todo.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private long id;
+	
+	@Column(nullable = false, unique = true)
 	private String username;
+	
+	@Column(nullable = false, unique = true)
 	private String password;
 	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
 	public String getUsername() {
 		return username;
 	}
