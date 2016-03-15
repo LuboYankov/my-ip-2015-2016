d98732e4c4a28ef1b33b6738004601c0c0779bd3
LuboYankov <ahhak123@gmail.com>
2016-03-15 09:10:09 +0200

    Added getMember() method to MembersService

--- 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/rest/MembersRest.java
+++ 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/rest/MembersRest.java
@@ -1,13 +1,18 @@
 package org.elsysbg.ip.todo.rest;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.elsysbg.ip.todo.entities.Member;
+import org.elsysbg.ip.todo.entities.Task;
 import org.elsysbg.ip.todo.services.MemberService;
 
 @Path("/members")
@@ -24,5 +29,13 @@ public class MembersRest {
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	public Member createMember(Member member) {
 		return membersService.createMember(member);
+	}
+	
+	@GET
+	@Path("/{memberid}/tasks")
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public List<Task> getMemberTasks(@PathParam("memberId") long memberId) {
+		final Member author = membersService.getMember(memberId);
+		return tasksService.getTasksByAuthor(author);
 	}
 }
--- 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/services/MemberService.java
+++ 05_SampleBackend/src/main/java/org/elsysbg/ip/todo/services/MemberService.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
 import org.elsysbg.ip.todo.entities.Member;
+import org.elsysbg.ip.todo.entities.Task;
 import org.elsysbg.ip.todo.entities.Member;
 
 @Singleton
@@ -39,6 +40,19 @@ public class MemberService {
 		try {
 			final TypedQuery<Member> query = em.createNamedQuery(Member.QUERY_ALL, Member.class);
 			return query.getResultList();
+		} finally {
+			em.close();
+		}
+	}
+	
+	public Member getMember(long memberId) {
+		final EntityManager em = entityManagerService.createEntityManager();
+		try {
+			final Member result = em.find(Member.class, memberId);
+			if(result == null) {
+				throw new IllegalArgumentException("no member with id: " + memberId);
+			}
+			return result;
 		} finally {
 			em.close();
 		}
